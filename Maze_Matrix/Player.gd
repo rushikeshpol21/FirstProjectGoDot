97eaa10fcf45923556996945dd12c9256360dece
kali <rmv.in123@gmail.com>
2021-01-25 22:12:53 +0530

    new changes

--- Maze_Matrix/Player.gd
+++ Maze_Matrix/Player.gd
@@ -1,30 +1,90 @@
 extends KinematicBody2D
-#var velocity = 100
-#var speed = 100
+const ACCELERATION = 100
+const MAX_SPEED = 100
+const FRICTION =  500
+var velocity = Vector2.ZERO
 #var Mainsceen = preload("res://Main.tscn")
 onready var animationPlayer = $"../AnimationPlayer"
-#onready var animationTree = $AnimationTree
-var velocity = Vector2.ZERO
+onready var animationTree = $AnimationTree
+onready var animation_mode = animationTree.get("parameters/playback")
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/idle/blend_position", input_vector)
+		animationTree.set("parameters/walk/blend_position", input_vector)
+		animation_mode.travel("walk")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * FRICTION)
+	else:
+		animation_mode.travel("idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)		
+	
 func _ready():
 	pass
-func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -1
-		animationPlayer.play("Runleft")
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = 1
-		animationPlayer.play("Runright")
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = -1
-		animationPlayer.play("Runup")
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 1
-		animationPlayer.play("Rundown")
-	else:
-		velocity.x = 0
-		velocity.y = 0
-		animationPlayer.play("idleleft")
-	move_and_collide(velocity)
+
+
+
+
+
+
+
+
+
+
+
+
+	
+#
+#export (int) var speed = 100
+#
+#var velocity = Vector2()
+#func get_input():
+#	velocity = Vector2()
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
+#	velocity = velocity.normalized() * speed
+#
+#func _physics_process(delta):
+#	get_input()
+#	velocity = move_and_slide(velocity)
+
+
+
+
+
+
+	
+
+#func _physics_process(delta):
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x = -1
+#		animationPlayer.play("Runleft")
+#	elif Input.is_action_pressed("ui_right"):
+#		velocity.x = 1
+#		animationPlayer.play("Runright")
+#	elif Input.is_action_pressed("ui_up"):
+#		velocity.y = -1
+#		animationPlayer.play("Runup")
+#	elif Input.is_action_pressed("ui_down"):
+#		velocity.y = 1
+#		animationPlayer.play("Rundown")
+#	else:
+#		velocity.x = 0
+#		velocity.y = 0
+#		animationPlayer.play("idleleft")
+#	move_and_collide(velocity.normalized()*delta*speed)
 		
 	
 	
