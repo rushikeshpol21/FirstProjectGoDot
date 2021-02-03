f5afd0f09b2ea6a377c6eea747b6913a8d5a537f
kali <rmv.in123@gmail.com>
2021-02-03 16:36:42 +0530

    Player wall collision issue resolve

--- Maze_Matrix/Player.gd
+++ Maze_Matrix/Player.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/idle/blend_position", input_vector)
 		animationTree.set("parameters/walk/blend_position", input_vector)
@@ -22,9 +22,9 @@ func _physics_process(delta):
 	else:
 		animation_mode.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		
+
 	velocity = move_and_slide(velocity)		
-	
+
 func _ready():
 	pass
 
@@ -40,7 +40,7 @@ func _ready():
 
 
 	
-#
+
 #export (int) var speed = 100
 #
 #var velocity = Vector2()
@@ -66,24 +66,27 @@ func _ready():
 
 
 	
-
+#var velocity = Vector2()
+#var speed = 100
+#
 #func _physics_process(delta):
+#
 #	if Input.is_action_pressed("ui_left"):
 #		velocity.x = -1
-#		animationPlayer.play("Runleft")
+#		#animationPlayer.play("Runleft")
 #	elif Input.is_action_pressed("ui_right"):
 #		velocity.x = 1
-#		animationPlayer.play("Runright")
+#		#animationPlayer.play("Runright")
 #	elif Input.is_action_pressed("ui_up"):
 #		velocity.y = -1
-#		animationPlayer.play("Runup")
+#		#animationPlayer.play("Runup")
 #	elif Input.is_action_pressed("ui_down"):
 #		velocity.y = 1
-#		animationPlayer.play("Rundown")
+#		#animationPlayer.play("Rundown")
 #	else:
 #		velocity.x = 0
 #		velocity.y = 0
-#		animationPlayer.play("idleleft")
+#		#animationPlayer.play("idleleft")
 #	move_and_collide(velocity.normalized()*delta*speed)
 		
 	
@@ -107,18 +110,17 @@ func _ready():
 	
 	
 	
-	
-	
-	
-	
+#var velocity = Vector2()
+#var Max_Speed = 100
+#func _process(delta):
 #	var input_vector = Vector2.ZERO
 #	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 #	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 #	input_vector = input_vector.normalized()
 #
 #	if input_vector != Vector2.ZERO:
-#		animationTree.set("parameters/idle/blend_position", input_vector)
-#		animationTree.set("parameters/run/blend_position", input_vector)		
+#		#animationTree.set("parameters/idle/blend_position", input_vector)
+#		#animationTree.set("parameters/run/blend_position", input_vector)		
 #		velocity = input_vector * Max_Speed
 #	else:
 #		velocity = Vector2.ZERO
