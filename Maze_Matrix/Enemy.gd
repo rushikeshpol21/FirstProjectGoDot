b3d16b4fbd4497302b90f18ea4158aa28f0d467c
Rushikesh <rushikeshpol21@gmail.com>
2021-02-03 21:26:18 +0530

    Enemy movement

--- Maze_Matrix/Enemy.gd
+++ Maze_Matrix/Enemy.gd
@@ -1,55 +1,21 @@
 extends KinematicBody2D
 
-onready var animationPlayer = $AnimationPlayer
-onready var animationTree = $AnimationTree
-onready var animation_mode = animationTree.get("parameters/playback")
+var velocity = Vector2.ZERO
+var direction = 1
+const MAX_SPEED = 50
+
+
 
 func _ready():
 	pass
 
-enum {
-	IDLE,
-	WANDER
-}
-
-var velocity = Vector2.ZERO
-var state = IDLE
-
-const ACCELERATION = 300
-const MAX_SPEED = 50
-const TOLERANCE = 4.0
-
-onready var start_position = global_position
-onready var target_position = global_position
-
-func update_target_position():
-	var target_vector = Vector2(rand_range(-32, 32), rand_range(-32, 32))
-	target_position = start_position + target_vector
-
-func is_at_target_position(): 
-	# Stop moving when at target +/- tolerance
-	return (target_position - global_position).length() < TOLERANCE
-
 func _physics_process(delta):
-	match state:
-		IDLE:
-			state = WANDER
-			# Maybe wait for X seconds with a timer before moving on
-			update_target_position()
-
-		WANDER:
-			accelerate_to_point(target_position, ACCELERATION * delta)
-
-			if is_at_target_position():
-				state = IDLE
-
+	velocity.y = MAX_SPEED * direction
+	if direction == 1:
+		$Sprite.flip_v = false
+	else:
+		$Sprite.flip_v = true
 	velocity = move_and_slide(velocity)
 
-func accelerate_to_point(point, acceleration_scalar):
-	var direction = (point - global_position).normalized()
-	var acceleration_vector = direction * acceleration_scalar
-	accelerate(acceleration_vector)
-
-func accelerate(acceleration_vector):
-	velocity += acceleration_vector
-	velocity = velocity.clamped(MAX_SPEED)
+	if is_on_wall():
+		direction = direction * -1
