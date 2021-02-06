9e11fba2ca7cccafcd0826ca2e778f375ae32798
kali <rmv.in123@gmail.com>
2021-02-06 17:57:33 +0530

    touch screen updated

---
+++ Maze_Matrix/TouchscreenCamera.gd
@@ -0,0 +1,34 @@
+extends Camera2D
+export (NodePath) var target
+
+# Optional: export these properties for convenient editing.
+
+var target_return_enabled = false
+var target_return_rate = 0.02
+var min_zoom = 0.5
+var max_zoom = 2
+var zoom_sensitivity = 10
+var zoom_speed = 0.05
+
+var events = {}
+var last_drag_distance = 0
+
+
+func _process(delta):
+	if target and target_return_enabled and events.size() == 0:
+		position = lerp(position, get_node(target).position, target_return_rate)
+		
+		
+func _unhandled_input(event):
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			events[event.index] = event
+		else:
+			events.erase(event.index)
+
+	if event is InputEventScreenDrag:
+		events[event.index] = event
+		if events.size() == 1:
+			position += event.relative * zoom.x
+
+
