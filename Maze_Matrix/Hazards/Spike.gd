a49be24a5d2cfb10ae710f81ecce0ebd82757dfc
rahul951we <77875459+rahul951we@users.noreply.github.com>
2021-02-24 21:12:23 +0530

    spike changes

--- Maze_Matrix/Hazards/Spike.gd
+++ Maze_Matrix/Hazards/Spike.gd
@@ -1,6 +1,22 @@
 extends Area2D
 
+var count = 0
+onready var timer = get_node("Timer")
+
+func _ready():
+	timer.set_wait_time(2)
+	timer.start()
+	$AnimatedSprite.play()
 
 
-func _on_Spike_body_entered(body):
-	print("hello")
+func _on_Timer_timeout():
+	count += 1
+#	print(count)
+	showhidetile()
+	
+func showhidetile():
+	if count % 2 == 0 :
+		$AnimatedSprite.hide()
+	else:
+		$AnimatedSprite.show()
+		$AnimatedSprite.play()
